0a7e4313acdb89fe0deb755012505a7a1b02534d
DhruvBhardwaj734 <dhruvbhardwaj734@gmail.com>
2026-09-19 16:57:16 +0530

    dealer detail add function added

--- watsoo_tracking.py
+++ watsoo_tracking.py
@@ -10,12 +10,15 @@ with sync_playwright() as p:
         page.get_by_placeholder("Enter your password").fill(password)
         page.get_by_role("button", name= "Sign In").click()
     Login(Username, password)
-    page.locator(".sidebar").hover()
-    Vm = page.get_by_text("Vehicle Management",exact=True).click()
-    page.get_by_role("button",name="Add Vehicle").click()
+
+   
     vn = input("Enter Vehicle number: ")
     imei = input("Enter imei ")
+
     def add_details(vn,imei):
+        page.locator(".sidebar").hover()
+        page.get_by_text("Vehicle Management",exact=True).click()
+        page.get_by_role("button",name="Add Vehicle").click()
         page.get_by_placeholder("Enter Vehicle Number").fill(vn)
         page.get_by_text("Device Configures").click()
         page.locator('input[placeholder="Select IoT Devices"]').click()
@@ -25,5 +28,19 @@ with sync_playwright() as p:
         page.get_by_text("Driver Details", exact=True).click()
         page.get_by_role("button", name="Save Vehicle").click()
     add_details(vn, imei)
+    Name = input("Enter Dealer Name:")
+    Email = input("Enter Dealer Email:")
+    Phone_number = input("Enter Dealer Phone Number:")
+    def user_add(Name,Email,Phone_number):
+        page.locator(".sidebar").hover()
+        page.get_by_text("User Management", exact=True).click()
+        page.get_by_text("Dealer Management", exact=True).click()
+        page.get_by_role("button", name = "Add Dealer").click()
+        page.get_by_placeholder("Enter Name").fill(Name)
+        page.get_by_placeholder("Enter Email").fill(Email)
+        page.get_by_placeholder("Enter Phone").fill(Phone_number)
+        page.get_by_placeholder("Official Email").fill(Email)
+    user_add(Name,Email,Phone_number)
+
     input("Press Enter to close the browser...")
     browser.close()
